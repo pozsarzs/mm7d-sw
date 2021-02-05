0223d8cd418231d2d98d061207dbb34db75983dc
Pozsár Zsolt <pozsarzs@karak>
2021-02-05 21:46:57 +0100

    210205

--- source/mm7d.ino
+++ source/mm7d.ino
@@ -1,6 +1,6 @@
 // +---------------------------------------------------------------------------+
-// | MM7D v0.1 * Air quality measuring device                                  |
-// | Copyright (C) 2020 Pozsár Zsolt <pozsar.zsolt@szerafingomba.hu>           |
+// | MM7D v0.2 * Air quality measuring device                                  |
+// | Copyright (C) 2020-2021 Pozsár Zsolt <pozsar.zsolt@szerafingomba.hu>      |
 // | mm7d.ino                                                                  |
 // | Program for ESP8266 Huzzah Breakout                                       |
 // +---------------------------------------------------------------------------+
@@ -21,10 +21,10 @@
 #define TYP_SENSOR1 DHT11
 
 // settings
-const char* wifi_ssid       = "";  // SSID of Wi-Fi AP
-const char* wifi_password   = "";  // password of Wi-Fi AP
-const String uid            = "";  // user ID
-const String allowedaddress = "";  // client IP addresses with space delimiter
+const char* wifi_ssid       = "SzerafinGomba";  // SSID of Wi-Fi AP
+const char* wifi_password   = "Hal4csk4Macsk4";  // password of Wi-Fi AP
+const String uid            = "B0b0rj4n"; // user ID
+const String allowedaddress = "192.168.1.8 192.168.1.9 192.168.1.10";  // client IP addresses with space delimiter
 
 // GPIO ports
 const int prt_buzzer        = 14;
@@ -73,7 +73,7 @@ const String msg31          = "* E05: Page not found!";
 // general constants
 const int maxadcvalue       = 1024;
 const long interval         = 2000;
-const String swversion      = "0.1";
+const String swversion      = "0.2";
 
 // variables
 float humidity, temperature, unwantedgaslevel;
@@ -84,6 +84,9 @@ String line;
 String localipaddress;
 unsigned long prevtime1     = 0;
 unsigned long prevtime2     = 0;
+int h1, h2, h3, h4;
+int t1, t2, t3, t4;
+int g;
 
 DHT dht(prt_sensor1, TYP_SENSOR1, 11);
 ESP8266WebServer server(80);
@@ -131,33 +134,89 @@ void setup(void)
   // start webserver
   Serial.print(msg12);
   server.onNotFound(handleNotFound);
+  // write help page as html text
   server.on("/", []()
   {
     writeclientipaddress();
-    line = "<html><head><title>" + msg01 + "</title></head>"
-           "<body bgcolor=\"#e2f4fd\"><h2>" + msg01 + "</h2>""<br>"
-           "Software version: v" + swversion + "<br>"
-           "<hr><h3>Plain text data and control pages:</h3><br>"
-           "<table border=\"0\" cellpadding=\"5\">"
-           "<tr><td><a href=\"http://" + localipaddress + "/version\">http://" + localipaddress + "/version</a></td><td>Get software name and version</td></tr>"
-           "<tr><td colspan=\"2\">&nbsp;</td></tr>"
-           "<tr><td><a href=\"http://" + localipaddress + "/get/all\">http://" + localipaddress + "/get/all</a></td><td>Get all data<sup>*</sup></td></tr>"
-           "<tr><td><a href=\"http://" + localipaddress + "/get/humidity\">http://" + localipaddress + "/get/humidity</a></td><td>Get relative humidity in %<sup>*</sup></td></tr>"
-           "<tr><td><a href=\"http://" + localipaddress + "/get/temperature\">http://" + localipaddress + "/get/temperature</a></td><td>Get temperature in &deg;C<sup>*</sup></td></tr>"
-           "<tr><td><a href=\"http://" + localipaddress + "/get/unwantedgaslevel\">http://" + localipaddress + "/get/unwantedgaslevel</a></td><td>Get relative level of unwanted gases in %<sup>*</sup></td></tr>"
-           "<tr><td colspan=\"2\">&nbsp;</td></tr>"
-           "<tr><td><a href=\"http://" + localipaddress + "/set/all/off\">http://" + localipaddress + "/set/all/off</a></td><td>Switch off all LEDs<sup>*</sup></td></tr>"
-           "<tr><td><a href=\"http://" + localipaddress + "/set/greenled/off\">http://" + localipaddress + "/set/greenled/off</a></td><td>Switch off green LED<sup>*</sup></td></tr>"
-           "<tr><td><a href=\"http://" + localipaddress + "/set/greenled/on\">http://" + localipaddress + "/set/greenled/on</a></td><td>Switch on green LED<sup>*</sup></td></tr>"
-           "<tr><td><a href=\"http://" + localipaddress + "/set/yellowled/off\">http://" + localipaddress + "/set/yellowled/off</a></td><td>Switch off yellow LED<sup>*</sup></td></tr>"
-           "<tr><td><a href=\"http://" + localipaddress + "/set/yellowled/on\">http://" + localipaddress + "/set/yellowled/on</a></td><td>Switch on yellow LED<sup>*</sup></td></tr>"
-           "<tr><td><a href=\"http://" + localipaddress + "/set/redled/off\">http://" + localipaddress + "/set/redled/off</a></td><td>Switch off red LED<sup>*</sup></td></tr>"
-           "<tr><td><a href=\"http://" + localipaddress + "/set/redled/on\">http://" + localipaddress + "/set/redled/on</a></td><td>Switch on red LED<sup>*</sup></td></tr>"
-           "</table><br><sup>*</sup>Use <i>uid</i> argument!<br>"
-           "<hr><center>" + msg02 + " <a href=\"mailto:" + msg03 + "\">" + msg28 + "</a> - <a href=\"" + msg04 + "\">Homepage</a><center><br><body></html>";
+    line = "<html>\n"
+           "  <head>\n"
+           "    <title>" + msg01 + "</title>\n"
+           "  </head>\n"
+           "  <body bgcolor=\"#e2f4fd\">\n"
+           "    <h2>" + msg01 + "</h2>\n"
+           "    <br>\n"
+           "    Software version: v" + swversion + "<br>\n"
+           "    <hr>\n"
+           "    <h3>Plain text data and control pages:</h3>\n"
+           "    <br>\n"
+           "    <table border=\"0\" cellpadding=\"5\">\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/version</td>\n"
+           "        <td>Get software name and version</td>\n"
+           "      </tr>\n"
+           "      <tr><td colspan=\"2\">&nbsp;</td></tr>\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/operation?uid=abcdef&g=20&h1=65&h2=70&h3=80&h4=85&t1=13&t2=150&t3=20&t4=22</td>\n"
+           "        <td>Get all data and set limit values to switch on/off LEDs</td>\n"
+           "      </tr>\n"
+           "      <tr><td colspan=\"2\">&nbsp;</td></tr>\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/get/all?uid=abcdef</a></td>\n"
+           "        <td>Get all data<sup>*</sup></td>\n"
+           "      </tr>\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/get/humidity?uid=abcdef</a></td>\n"
+           "        <td>Get relative humidity in %<sup>*</sup></td>\n"
+           "      </tr>\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/get/temperature?uid=abcdef</a></td>\n"
+           "        <td>Get temperature in &deg;C</td>\n"
+           "      </tr>\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/get/unwantedgaslevel?uid=abcdef</a></td>\n"
+           "        <td>Get relative level of unwanted gases in %</td>\n"
+           "      </tr>\n"
+           "      <tr>\n"
+           "        <td colspan=\"2\">&nbsp;</td></tr>"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/set/all/off?uid=abcdef</a></td>\n"
+           "        <td>Switch off all LEDs</td>\n"
+           "      </tr>\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/set/greenled/off?uid=abcdef</a></td>\n"
+           "        <td>Switch off green LED</td>\n"
+           "      </tr>\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/set/greenled/on?uid=abcdef</a></td>\n"
+           "        <td>Switch on green LED</td>\n"
+           "      </tr>\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/set/yellowled/off?uid=abcdef</a></td>\n"
+           "        <td>Switch off yellow LED</td>\n"
+           "      </tr>\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/set/yellowled/on?uid=abcdef</a></td>\n"
+           "        <td>Switch on yellow LED</td>\n"
+           "      </tr>\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/set/redled/off?uid=abcdef</a></td>\n"
+           "        <td>Switch off red LED</td>\n"
+           "      </tr>\n"
+           "      <tr>\n"
+           "        <td>http://" + localipaddress + "/set/redled/on?uid=abcdef</td>\n"
+           "        <td>Switch on red LED</td>\n"
+           "      </tr>\n"
+           "    </table>\n"
+           "    <br>\n"
+           "    <hr>\n"
+           "    <center>" + msg02 + " <a href=\"mailto:" + msg03 + "\">" + msg28 + "</a> - <a href=\"" + msg04 + "\">Homepage</a><center>\n"
+           "    <br>\n"
+           "  <body>\n"
+           "</html>\n";
     server.send(200, "text/html", line);
     delay(100);
   });
+  // write software version as plain text
   server.on("/version", []()
   {
     writeclientipaddress();
@@ -165,10 +224,53 @@ void setup(void)
     server.send(200, "text/plain", line);
     delay(100);
   });
+  // write all measured parameters as plain text and set limit values
+  server.on("/operation", []()
+  {
+    if (checkipaddress() == 1)
+      if (checkuid() == 1)
+      {
+        getunwantedgaslevel();
+        gettemphum();
+        int green = 0;
+        int yellow = 0;
+        int red = 0;
+        if ((setlimitvalues == 0) && (unwantedgaslevel != 999) && (humidity != 999) && (temperature != 999))
+        {
+          if ((int)unwantedgaslevel < g) green = 1;
+          if ((int)unwantedgaslevel == g) yellow = 1;
+          if ((int)unwantedgaslevel > g) red = 1;
+          if ((int)humidity < h1) red = 1;
+          if (((int)humidity > h1) || ((int)humidity < h2)) yellow = 1;
+          if (((int)humidity > h2) || ((int)humidity < h3)) green = 1;
+          if (((int)humidity > h3) || ((int)humidity < h4)) yellow = 1;
+          if ((int)humidity > h4) red = 1;
+          if ((int)temperature < t1) red = 1;
+          if (((int)temperature > t1) || ((int)humidity < t2)) yellow = 1;
+          if (((int)temperature > t2) || ((int)humidity < t3)) green = 1;
+          if (((int)temperature > t3) || ((int)humidity < t4)) yellow = 1;
+          if ((int)temperature > t4) red = 1;
+          if (red == 1)
+          {
+            green = 0;
+            yellow = 0;
+          }
+        }
+        greenled(green);
+        yellowled(yellow);
+        redled(red);
+        line = String((int)unwantedgaslevel) + "\n" + String((int)humidity) + "\n" + String((int)temperature);
+        server.send(200, "text/plain", line);
+      } else
+      {
+        server.send(401, "text/plain", msg17);
+        Serial.println(msg18);
+      }
+  });
+  // write all measured parameters as plain text
   server.on("/get/all", []()
   {
     if (checkipaddress() == 1)
-    {
       if (checkuid() == 1)
       {
         getunwantedgaslevel();
@@ -180,12 +282,11 @@ void setup(void)
         server.send(401, "text/plain", msg17);
         Serial.println(msg18);
       }
-    }
   });
+  // write relative unwanted gas level as plain text
   server.on("/get/unwantedgaslevel", []()
   {
     if (checkipaddress() == 1)
-    {
       if (checkuid() == 1)
       {
         getunwantedgaslevel();
@@ -196,12 +297,11 @@ void setup(void)
         server.send(401, "text/plain", msg17);
         Serial.println(msg18);
       }
-    }
   });
+  // write relative humidity as plain text
   server.on("/get/humidity", []()
   {
     if (checkipaddress() == 1)
-    {
       if (checkuid() == 1)
       {
         gettemphum();
@@ -212,12 +312,11 @@ void setup(void)
         server.send(401, "text/plain", msg17);
         Serial.println(msg18);
       }
-    }
   });
+  // write temperature in degree Celsius as plain text
   server.on("/get/temperature", []()
   {
     if (checkipaddress() == 1)
-    {
       if (checkuid() == 1)
       {
         gettemphum();
@@ -228,12 +327,11 @@ void setup(void)
         server.send(401, "text/plain", msg17);
         Serial.println(msg18);
       }
-    }
   });
+  // switch off all LEDs
   server.on("/set/all/off", []()
   {
     if (checkipaddress() == 1)
-    {
       if (checkuid() == 1)
       {
         greenled(0);
@@ -245,12 +343,11 @@ void setup(void)
         server.send(401, "text/plain", msg17);
         Serial.println(msg18);
       }
-    }
   });
+  // switch off green LED
   server.on("/set/greenled/off", []()
   {
     if (checkipaddress() == 1)
-    {
       if (checkuid() == 1)
       {
         greenled(0);
@@ -260,12 +357,11 @@ void setup(void)
         server.send(401, "text/plain", msg17);
         Serial.println(msg18);
       }
-    }
   });
+  // switch on green LED
   server.on("/set/greenled/on", []()
   {
     if (checkipaddress() == 1)
-    {
       if (checkuid() == 1)
       {
         greenled(1);
@@ -275,12 +371,11 @@ void setup(void)
         server.send(401, "text/plain", msg17);
         Serial.println(msg18);
       }
-    }
   });
+  // switch off red LED
   server.on("/set/redled/off", []()
   {
     if (checkipaddress() == 1)
-    {
       if (checkuid() == 1)
       {
         redled(0);
@@ -290,12 +385,11 @@ void setup(void)
         server.send(401, "text/plain", msg17);
         Serial.println(msg18);
       }
-    }
   });
+  // switch on red LED
   server.on("/set/redled/on", []()
   {
     if (checkipaddress() == 1)
-    {
       if (checkuid() == 1)
       {
         redled(1);
@@ -305,12 +399,11 @@ void setup(void)
         server.send(401, "text/plain", msg17);
         Serial.println(msg18);
       }
-    }
   });
+  // switch off yellow LED
   server.on("/set/yellowled/off", []()
   {
     if (checkipaddress() == 1)
-    {
       if (checkuid() == 1)
       {
         yellowled(0);
@@ -320,12 +413,11 @@ void setup(void)
         server.send(401, "text/plain", msg17);
         Serial.println(msg18);
       }
-    }
   });
+  // switch on yellow LED
   server.on("/set/yellowled/on", []()
   {
     if (checkipaddress() == 1)
-    {
       if (checkuid() == 1)
       {
         yellowled(1);
@@ -335,11 +427,10 @@ void setup(void)
         server.send(401, "text/plain", msg17);
         Serial.println(msg18);
       }
-    }
   });
   server.begin();
   Serial.println(msg08);
-  beep();
+  beep(1);
 }
 
 // error 404
@@ -408,7 +499,7 @@ void getunwantedgaslevel()
     unwantedgaslevel = adcvalue / (maxadcvalue / 100);
     if (unwantedgaslevel > 100)
     {
-      beep();
+      beep(1);
       Serial.println(msg14);
       unwantedgaslevel = 999;
       return;
@@ -427,7 +518,7 @@ void gettemphum()
     temperature = dht.readTemperature(false);
     if (isnan(humidity) || isnan(temperature))
     {
-      beep();
+      beep(1);
       Serial.println(msg15);
       temperature = 999;
       humidity = 999;
@@ -456,44 +547,63 @@ int checkipaddress()
   for (int i = 0; i < itemCount; i++)
   {
     String item = splitter->getItemAtIndex(i);
-    if (clientaddress == String(item))
-    {
-      allowed = 1;
-    }
+    if (clientaddress == String(item)) allowed = 1;
   }
-  if (allowed == 1)
-  {
-    return 1;
-  } else
+  if (allowed == 1) return 1; else
   {
     server.send(401, "text/plain", msg19);
     Serial.println(msg20);
-    beep();
-    beep();
-    beep();
+    beep(3);
     return 0;
   }
+}
+
+// limit values
+int setlimitvalues()
+{
+  if (server.args() > 9)
+  {
+    String arg;
+    arg = server.arg("g");
+    if (arg.length() != 0) g = arg.toInt(); else return 1;
+    arg = server.arg("h1");
+    if (arg.length() != 0) h1 = arg.toInt(); else return 1;
+    arg = server.arg("h2");
+    if (arg.length() != 0) h2 = arg.toInt(); else return 1;
+    arg = server.arg("h3");
+    if (arg.length() != 0) h3 = arg.toInt(); else return 1;
+    arg = server.arg("h4");
+    if (arg.length() != 0) h4 = arg.toInt(); else return 1;
+    arg = server.arg("t1");
+    if (arg.length() != 0) t1 = arg.toInt(); else return 1;
+    arg = server.arg("t2");
+    if (arg.length() != 0) t2 = arg.toInt(); else return 1;
+    arg = server.arg("t3");
+    if (arg.length() != 0) t3 = arg.toInt(); else return 1;
+    arg = server.arg("t4");
+    if (arg.length() != 0) t4 = arg.toInt(); else return 1;
+  } else return 1;
+  return 0;
 }
 
 // authentication
 int checkuid()
 {
-  if (server.arg("uid") == uid)
+  if (server.arg("uid") == uid) return 1; else
   {
-    return 1;
-  } else
-  {
-    beep();
-    beep();
+    beep(2);
     return 0;
   }
 }
 
-// beep
-void beep()
+// beep sign
+void beep(int num)
 {
-  tone(prt_buzzer, 880);
-  delay (100);
-  noTone(prt_buzzer);
-  delay (100);
+  for (int i = 0; i < num; i++)
+  {
+    tone(prt_buzzer, 880);
+    delay (100);
+    noTone(prt_buzzer);
+    delay (100);
+  }
 }
